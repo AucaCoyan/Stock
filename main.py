2318436ed2bd59d49240bb7bad1266bd59bd785d
Auca <forosypaginas@gmail.com>
2020-03-06 17:18:27 -0300

    Adjusts on variables and classes. Some functions incorporated

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 """
-Dudas:
+Questions:
 A-Visual web or application
+
+to do:
+ui.py
+load different files
+load objects from an existent .csv
+
 """
+
 
 # -------------------------------------------------------------------------------
 #                                      SECTION
@@ -10,99 +17,127 @@ A-Visual web or application
 # -------------------SUBSECTION-------------------
 
 # -------------------Libraries & files-------------------
-# todo: load different files
 
 # -------------------Classes-------------------
 # import ui.py
 
 
 class Object:
-    def __init__(self, stock):
-        self.stock = 0
+    def __init__(self):  # When Object is created between () says the quantity
+        self.stock = input("Insert the initial quantity:")  # quantity assignation
+
     pass
 
-# Hardcoded Objects todo: load objects from an existent .csv
 
+# Hardcoded Objects
 
-pencil = Object(10)
+'''
+pencil = Object()
 print(pencil.stock)
 
 pencil.stock = 10
 
 print(pencil.stock)
+'''
+
+class Worker:
+    def __init__(self, ):
+        self.name = input("Insert the name of the worker:")
+        self.sector = input("Insert the sector of the worker:")
+    pass
 
 
 '''
-class Tool(Object):
-    pass
-
-
-class Worker:
-    def __init__(self, name, sector,):
-        self.name = name
-        self.sector = none
-    pass
-
 
 class Receipt:
     pass
-
 
 class Supplier:
     pass
 
 '''
 
+
 # -------------------Functions-------------------
 
-def addProduct():
-    print("A new product will be created")
-    productname = input("Name of the new product: ")
-    print(productname, " was created")
+
+def add_product():
+    print('A new product will be created')
+    product_name = input('Name of the new product: ')
+    product_quantity = int(input('Enter the initial quantity of the new product: '))
+    print(type(product_quantity))
+    product_name.Object = product_quantity + 1
+    print(product_name, ' was created. With ', product_quantity, ' items.')
     pass
+
+
+# .......................LOOKUPS...........................
+
+
+def product_lookup(product):
+    if try_product(product) is False:
+        raise
+    #   pass
+    else:
+        product = Object()
+        print("The Product is %s \n " % product.stock)
+    pass
+
+
+# .......................TRY EXISTS...........................
+
+def try_product(product):
+    try:
+        product.Object
+    except:
+        print("Product can't be found.")
+        return False
+    else:
+        return True
 
 
 # -------------------------------------------------------------------------------
 #                            Star of the program
 # -------------------------------------------------------------------------------
 
-print("Welcome to the stock management software. \n")
+
+print('Welcome to the stock management software. \n')
 while True:
-    userinput = input("--------------------------------------\n"
-                      "START. \n "
-                      "\n"
-                      "Choose an option: \n "
-                      "1-Add Product \n "
-                      "2-Crear producto \n"
-                      "3- \n"
-                      "Para finalizar, presione N\n"
-                      "\n"
-                      "Opcion a elegir: ")
-    if userinput == "1":
-        addProduct()
-    # if userinput == "2":
-    #     crearproducto()
-    # if userinput == "3":
+    user_input = input('--------------------------------------\n'
+                       'START. \n '
+                       '\n'
+                       'Choose an option: \n '
+                       '1-Add Product \n '
+                       '2-Look up for a product  \n'
+                       '  \n'
+                       'To exit, press N\n'
+                       '\n'
+                       'User input: ')
+    if user_input == "1":
+        add_product()
+    if user_input == "2":
+        product_lookup(input("Enter the name of the product to look up: "))
+    # if user_input == "3":
     #     creartransaccion()
-    # if userinput == "4":
+    # if user_input == "4":
     #     consultarcliente(int(input("Ingrese el CODIGO del cliente a buscar: ")))
-    # if userinput == "5":
+    # if user_input == "5":
     #     buscarcliente(input("Ingrese nombre/apellido/CUIT del cliente a buscar: "))
-    # if userinput == "6":
+    # if user_input == "6":
     #     borrarclientes(int(input("Ingrese el CODIGO del cliente a borrar: ")))
-    # if userinput == "7":
+    # if user_input == "7":
     #     borrarproducto(int(input("Ingrese el CODIGO del producto a borrar: ")))
-    # if userinput == "8":
+    # if user_input == "8":
     #     borrartransaccion(int(input("Ingrese el CODIGO de la transaccion a borrar: ")))
-    # if userinput == "9":
+    # if user_input == "9":
     #     consultartransaccion(int(input("Ingrese el CODIGO de la transaccion a buscar: ")))
-    # if userinput == "10":
+    # if user_input == "10":
     #     print(clientList)
-    # if userinput == "11":
+    # if user_input == "11":
     #     print(transaccionList)
-    # if userinput == "12":
+    # if user_input == "12":
     #     calculartransaccion(int(input("Ingrese el CODIGO de la transaccion a calcular: ")))
-    # if userinput == "13":
+    # if user_input == "13":
     #     calculardeudacliente(int(input("Ingrese el CODIGO del cliente a calcular su deuda: ")))
-    if userinput == "n" or userinput == "N":
+    if user_input == "n" or user_input == "N":
         break
